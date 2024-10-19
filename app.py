510b226dd0ebc0601583864aff9a75a838f89127
k2421147c <k2421147c@e.ntu.edu.sg>
2024-10-19 05:40:39 +0000

    1

--- app.py
+++ app.py
@@ -20,3 +20,4 @@ if __name__ == "__main__":  # confirm if the programme run by developer
 # backend to communicate frontend in python to run, need protocol WSGI and need gunicorn (programme) to run WSGI
 # WSGI communicate between backend and frontend text, so need to convert to number
 # result r = model coeff * input by user + intercept
+# need to sync (left linkage) to update to github
